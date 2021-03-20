78a9e625f8aaa5a4c73426c600a92d2107e575fa
Arthur <fastfodd4@yandex.ru>
2021-03-20 20:34:26 +0300

    add pagination

--- availability/views.py
+++ availability/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 16)
+    page = request.GET.get('page')
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+        paginator = Paginator(products, 16)
+        try:
+            products = paginator.page(page)
+        except PageNotAnInteger:
+            products = paginator.page(1)
+        except EmptyPage:
+            products = paginator.page(paginator.num_pages)
+    else:
+        paginator = Paginator(products, 16)
+        try:
+            products = paginator.page(page)
+        except PageNotAnInteger:
+            products = paginator.page(1)
+        except EmptyPage:
+            products = paginator.page(paginator.num_pages)
     return render(request,
                   'availability/availability.html',
                   {'category': category,
